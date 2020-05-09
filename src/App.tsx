ddf49a76ac2ecdca34b29cd92deeef2ae00224e8
Alex Rothenberg <alexroth96@gmail.com>
2020-05-09 13:37:11 -0400

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,19 @@ import EstateAndTrustPage from './components/pages/EstateTrustPage';
 import BankruptcyPage from './components/pages/BankruptcyPage';
 import AboutPage from './components/pages/AboutPage';
 import SurveyPage from './components/pages/SurveyPage';
+import ReactGa from 'react-ga';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+
+ReactGa.initialize('UA-166020865-1');
+ReactGa.pageview('/');
+ReactGa.pageview('/estateplanning');
+ReactGa.pageview('/estateadmin');
+ReactGa.pageview('/survey');
+ReactGa.pageview('/bankruptcy');
+ReactGa.pageview('/estateandtrust');
+ReactGa.pageview('/about');
 
 const App: React.FunctionComponent = () => {
 	return (
